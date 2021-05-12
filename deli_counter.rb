5bb8d620758bb9ed7e814d196f621d4dfc51e93c
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-12 15:00:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+    
+  else 
+    new_line = katz_deli.collect.with_index(1) do |name, index|
+      id="#{index}. #{name}"
+      return id
+      puts "The line is currently: #{new_line}."
+    end
+  end
+end
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{line} in line."
+end
+
+def now_serving
+end
