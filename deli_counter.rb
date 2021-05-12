73bdf794f2f04e9e45fe9bd2a9f8de5e631b8edb
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-12 15:59:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    
+    katz_deli.each do |name|
+      katz_deli.unshift
   end
 end
