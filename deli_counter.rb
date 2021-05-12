e9c5e70de56a91e3795363412392190c876721db
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-12 15:33:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 def take_a_number(katz_deli, index)
   if katz_deli.length == 0
     
-  katz_deli.each_with_index do |name, index|
+  else katz_deli.each_with_index do |name, index|
     puts "Welcome, #{name}. You are number #{index} in line."
   end
 end
